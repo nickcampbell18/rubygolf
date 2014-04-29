0be2823f5963a2feef9cf634951e9e4441c85485
JGW Maxwell <jgwmaxwell@gmail.com>
2014-04-29 19:10:29 +0100

    Hole 2, hole 4

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,34 +1,50 @@
 class Golf
+  class << self
+    def hole1(a)
+      a.inject :*
+    end
 
-  def self.hole1(a)
-    a.inject :*
-  end
+  	def hole2
+      ('a'..'z').to_a
+    end
 
-  def self.hole3(n)
-    (1..n).reduce(:*)
-  end
+    def hole3(n)
+      (1..n).reduce(:*)
+    end
 
-  def self.hole5(a)
-    a.size.times.flat_map {|i|
-      a.each_cons(i+1).map.to_a
-    }
-  end
-
-  def self.hole6(n)
-    (1..n).map{|x|
-      case
-      when x%15 == 0
-        'fizzbuzz'
-      when x%3 == 0
-        'fizz'
-      when x%5 == 0
-        'buzz'
-      else
-        x
+    def hole4(a)
+      r1 = /\Acat\(/
+      a.map do |x|
+        case x
+        when /\Aman\(/
+          "hat(#{x})"
+        when r1
+          x.gsub r1, 'dead('
+        when /\Adog\(/
+          "#{x[0..-2]}(bone))"
+        end
       end
-    }
+    end
 
-  def self.hole7(a)
+    def hole5(a)
+      a.size.times.flat_map {|i|
+        a.each_cons(i+1).map { |x| x }
+      }
+    end
 
+    def hole6(n)
+      (1..n).map{|x|
+        case
+        when x%15 == 0
+          'fizzbuzz'
+        when x%3 == 0
+          'fizz'
+        when x%5 == 0
+          'buzz'
+        else
+          x
+        end
+      }
+    end
   end
 end
