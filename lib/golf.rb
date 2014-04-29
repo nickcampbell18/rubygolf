cfe63dae6a39b74a242e48818b494fc9f60c4a00
Mario Caropreso <mcaropreso@yammer-inc.com>
2014-04-29 19:10:12 +0100

    3 and 6

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,2 +1,22 @@
 class Golf
+
+  def self.hole3(n)
+    (1..n).reduce(:*)
+  end
+
+  def self.hole6(n)
+    (1..n).map{|x|
+      case
+      when x%15 == 0
+        'fizzbuzz'
+      when x%3 == 0
+        'fizz'
+      when x%5 == 0
+        'buzz'
+      else
+        x
+      end
+    }
+  end
+
 end
